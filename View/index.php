6193ed518d94ead49c5a70651cb8792f0038ed14
Ilya Marchenkov <mar4enkov@inbox.ru>
2021-03-08 15:36:13 +0400

    Added dir View.

---
+++ View/index.php
@@ -0,0 +1,40 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Онлайн библиотека, читать онлайн">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Онлайн библиотека">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <header class="row">
+        <div class="col-2">
+            <div class="row"> <img class="logo" src="img/logo.png" alt=""> </div>
+        </div>
+        <div class="col-6">
+            <div class="row">
+                <div class="title"><p>Онлайн читалка</p></div>
+            </div>
+        </div>
+        <div class="col-4 contacts">
+            <div class="row"><a href="mailto: vgbs@inbox.ru" class="email">books@inbox.ru</a> </div>
+            <div class="row"> <a href="tel: 88442653922" class="tel">8 (8442) 888888</a> </div>
+        </div>
+    </header>
+    <main></main>
+    <footer></footer>
+</div>
+</body>
+</html>
+
